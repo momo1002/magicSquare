74748324125bbeb5cf34bb9f8b46e3af86f31cd2
momo1002 <momoko.nakada1002@gmail.com>
2017-04-09 15:58:44 -0700

    Update MagicSquare4.java

--- MagicSquare4.java
+++ MagicSquare4.java
@@ -4,7 +4,7 @@ public class MagicSquare4 {
 
 		int[] row = new int[4];
 		int[] col = new int[4];
-		int k=0;
+		int k = 0;
 		int l = 0;
 		boolean resultC = true;
 		boolean resultR = true;
@@ -26,7 +26,7 @@ public class MagicSquare4 {
 			for(int i = 0; i < FourTimesFour.length ; i++){
 				for(int j = 0; j < FourTimesFour.length ; j++){
 					row[i] += Integer.parseInt(FourTimesFour[i][j]);
-					col[i] += Integer.parseInt(FourTimesFour[j][i]);
+					col[j] += Integer.parseInt(FourTimesFour[j][i]);
 				}
 			}
 
